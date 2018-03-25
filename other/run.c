e514c39c4bc5d86bd9a5c2f4c5bc4aa770b41b8e
liubailin2017 <liubailinprivate@163.com>
2018-03-25 16:46:29 +0800

    优化c语言程序

--- other/run.c
+++ other/run.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<string.h>
+#include<stdlib.h>
 
 int getPathlen(char *path) {
 	int i= 0;
@@ -8,15 +9,47 @@ int getPathlen(char *path) {
 	}
 	return i;
 }
+/*
+char* openPath(){  
+	char *buf = malloc(255);
+	int i = 0;
+	FILE *file;
+	char ch = 0;
+	file = fopen("path.txt","r");
+	ch = fgetc(file);
+	while(!feof(file))
+	{
+		buf[i++] = ch;
+		ch = fgetc(file);
+		
+	}
+	buf[i] = '\0';
+	fclose(file);
+	return buf;
+}
+*/
+
+
+char * getpath(){
+	char *buf = malloc(255);
+	int i = 0;
+	for( i = 0; i < 255; i++){
+		buf[i] = 0;
+	}
+	strncpy(buf,_pgmptr,getPathlen(_pgmptr));
+	//printf("buf:%s",buf);
+	return buf;
+}
 
 int main(int argc,char* args[]) {
 	int i  = 0; 
-	//这是程序目录， 不知道怎么获得当前目录，自己填写吧
-
-	char dir[256] = "/home/liubailin/bin";
+	char *path = getpath();
+	
 	char comm[200] = {"java -Dfile.encoding=utf-8 -jar "};
-	strcat(comm,dir);
-	strcat(comm,"/s.jar ");
+	strcat(comm,path);
+	strcat(comm,"\\s.jar --force ");
+	free(path);
+	
 	for(i = 1; i < argc; i++){
 		strcat(comm," ");
 		strcat(comm,args[i]);
