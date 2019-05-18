7fdbb3f6f73e20055cd942f01e3738574234c5f6
liubailin <liubailinprivate@163.com>
2019-05-18 23:46:14 +0800

    修正启动器部分(c语言部分)代码.

--- other/run.c
+++ other/run.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+#define ABSOUTION 1
 
+#if ABSOUTION != 1
+#define getPath getPath
 int getPathlen(char *path) {
 	int i= 0;
 	for( i = strlen(path); i > 0; i--) { 
@@ -9,27 +12,6 @@ int getPathlen(char *path) {
 	}
 	return i;
 }
-/*
-char* openPath(){  
-	char *buf = malloc(255);
-	int i = 0;
-	FILE *file;
-	char ch = 0;
-	file = fopen("path.txt","r");
-	ch = fgetc(file);
-	while(!feof(file))
-	{
-		buf[i++] = ch;
-		ch = fgetc(file);
-		
-	}
-	buf[i] = '\0';
-	fclose(file);
-	return buf;
-}
-*/
-
-
 char * getpath(){
 	char *buf = malloc(255);
 	int i = 0;
@@ -37,17 +19,46 @@ char * getpath(){
 		buf[i] = 0;
 	}
 	strncpy(buf,_pgmptr,getPathlen(_pgmptr));
-	//printf("buf:%s",buf);
+	printf("buf:\n<%s>\n",buf);
+	return buf;
+}
+#endif
+#if ABSOUTION == 1
+#define getPath openPath
+#define TMPSIZE 256
+/*
+在path.txt指明绝对路径
+*/
+char* openPath(){
+	char *buf = malloc(TMPSIZE);
+	int i = 0;
+	FILE *file;
+	char ch = 0;
+	file = fopen("path.txt","rb");
+	if(file == 0) {
+		return 0;
+	}
+	ch = fgetc(file);
+	while(!feof(file) && (i < TMPSIZE - 1) )
+	{
+		if(ch != '\n') buf[i++] = ch;
+		ch = fgetc(file);
+	}
+	buf[i] = '\0';
+	fclose(file);
 	return buf;
 }
 
+
+#endif
 int main(int argc,char* args[]) {
 	int i  = 0; 
-	char *path = getpath();
+	char *path = getPath();
 	
-	char comm[200] = {"java -Dfile.encoding=utf-8 -jar "};
+	char comm[256] = {"java -Dfile.encoding=utf-8 -jar "};
 	strcat(comm,path);
-	strcat(comm,"\\s.jar --force ");
+	strcat(comm,"s.jar --force ");
+
 	free(path);
 	
 	for(i = 1; i < argc; i++){
